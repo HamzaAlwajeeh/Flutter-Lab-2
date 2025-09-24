c180f2c96d133f831ccf81b867e0664cbb50c931
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-24 13:57:07 +0300

    adding assets music

---
+++ lib/helper/assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsMusicMusic1
+  /// assets/music/music-1.mp3
+  static const String assetsMusicMusic1 = "assets/music/music-1.mp3";
+
+  /// Assets for assetsMusicMusic2
+  /// assets/music/music-2.mp3
+  static const String assetsMusicMusic2 = "assets/music/music-2.mp3";
+
+  /// Assets for assetsMusicMusic3
+  /// assets/music/music-3.mp3
+  static const String assetsMusicMusic3 = "assets/music/music-3.mp3";
+
+  /// Assets for assetsMusicMusic4
+  /// assets/music/music-4.mp3
+  static const String assetsMusicMusic4 = "assets/music/music-4.mp3";
+
+  /// Assets for assetsMusicMusic5
+  /// assets/music/music-5.mp3
+  static const String assetsMusicMusic5 = "assets/music/music-5.mp3";
+
+  /// Assets for assetsMusicMusic6
+  /// assets/music/music-6.mp3
+  static const String assetsMusicMusic6 = "assets/music/music-6.mp3";
+
+  /// Assets for assetsMusicMusic7
+  /// assets/music/music-7.mp3
+  static const String assetsMusicMusic7 = "assets/music/music-7.mp3";
+}
+
